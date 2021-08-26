57efae715489019d6c4038a56628b02b2b8a4408
Mark Voltaire <markjvoltaire@gmail.com>
2021-08-26 18:20:06 -0400

    addition added

--- app.js
+++ app.js
@@ -1,14 +1,21 @@
 // import functions
 
+import { add } from './functions.js';
+
 // reference needed DOM elements
 const addNumber1 = document.getElementById('add-number-1');
 const addNumber2 = document.getElementById('add-number-2');
 const addButton = document.getElementById('add-button');
 const addAnswer = document.getElementById('add-answer');
-console.log(addNumber1, addNumber2, addButton, addAnswer);
+console.dir(addNumber1, addNumber2, addButton, addAnswer);
 // set event listeners 
 addButton.addEventListener('click', ()=>{
-    console.log('userclicked');
+    
+    const nom1 = Number(addNumber1.value);
+    const nom2 = Number(addNumber2.value);
+    console.log('userclicked', nom1,nom2); 
+    addAnswer.textContent = add(nom1, nom2);
+ 
 });
 
 
--- functions.js
+++ functions.js
@@ -1,3 +1,4 @@
-export function add(){
-    
+export function add(nom1, nom2){
+    const sum = nom1 + nom2 ;
+    return sum;
 }
